4d6926a32eff3e213ff0e547b9f93c2d5efc2f77
Debebe <debebesh@gmail.com>
2025-08-21 09:50:07 +0100

    functions to sample costs and parameters included

--- R/utilities/utilities.R
+++ R/utilities/utilities.R
@@ -32,3 +32,54 @@ brktpc <- function(x, y, z) { # bracket %
   )
 }
 
+
+gamma_sample <- function(mean, sd, n=1) {
+  shape <- (mean / sd)^2
+  scale <- (sd^2) / mean
+  rgamma(n, shape = shape, scale = scale)
+}
+
+
+sample_gamma <- function(mean, sd = NULL, lo = NULL, hi = NULL, n = 1) {
+  # If sd not provided, compute from lo/hi
+  if (is.null(sd)) {
+    if (is.null(lo) || is.null(hi)) {
+      stop("Either sd or both lo and hi must be provided")
+    }
+    sd <- (hi - lo) / 3.92
+  }
+  
+  shape <- (mean / sd)^2
+  scale <- (sd^2) / mean
+  rgamma(n, shape = shape, scale = scale)
+}
+
+
+
+sample_truncn <- function(mu, lo, hi, n = 1) {
+  sd <- (hi - lo) / 3.92
+  rtruncnorm(n, a =0, b = Inf, mean = mu, sd = sd)
+}
+
+
+sample_beta <- function(mean, lo, hi, n = 1) {
+  sd <- (hi - lo) / 3.92
+  var <- sd^2
+  
+  tmp <- mean * (1 - mean) / var - 1
+  alpha <- mean * tmp
+  beta  <- (1 - mean) * tmp
+  
+  rbeta(n, shape1 = alpha, shape2 = beta)
+}
+
+#sample_beta(mean = 0.034, lo = 0.0187, hi = 0.060)
+
+
+
+
+
+
+
+
+
