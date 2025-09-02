9b0f58e48a26c9b6c2db19a228eed7b090eead4b
Pete Dodd <pjd@Petes-Mac-mini.local>
2025-09-02 06:52:22 +0100

    moving files

---
+++ R/utilities/uploadresults.R
@@ -0,0 +1,26 @@
+## =======================================
+## LOADING TO GOOGLE SHEETS (authors only)
+## =======================================
+rm(list = ls())
+
+library(here)
+library(data.table)
+library(glue)
+library(googlesheets4)
+
+## setup - only accessible to those with access to this sheet NOTE new
+yourl <- "https://docs.google.com/spreadsheets/d/1-byoo8axKl_G-t4trsUP8gpuhy63akx379egbOVJSPM/edit?gid=0#gid=0"
+shid <- as.character(as_sheets_id(yourl))
+
+
+## utility function
+upload.to.sheets <- function(filename, sheetid) {
+  fn <- glue(here("outputs/{filename}"))
+  tmp <- fread(file = fn)
+  sht <- gsub("\\.csv", "", filename)
+  write_sheet(tmp, sheetid, sheet = sht)
+}
+
+
+## read & upload relevant data
+upload.to.sheets("output_table.csv", shid)
