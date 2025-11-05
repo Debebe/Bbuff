c388f06dc95df40bd602f54e6c1e02e0dbe2ccd8
Debebe <debebesh@gmail.com>
2025-11-05 15:43:24 +0000

    updated togenerate new outputs and removing ploting scripts

--- R/panalysis.R
+++ R/panalysis.R
@@ -10,11 +10,15 @@ pacman::p_load(here,data.table, dplyr, tidyr, stringr, truncnorm,
 ## === data
 ## read in pre-prepared data
 gdp_inc_le_costs <- readRDS(file = here("data/gdp_inc_le_costs.rds"))
+
 tbinc <- fread("data/TB_burden_countries_2025-10-29.csv") %>%
   dplyr::filter(year==2023) %>%
-  dplyr::select(iso3, year,inc_all=e_inc_100k,  e_inc_100k_lo, e_inc_100k_hi)%>%as.data.table()
+  dplyr::select(iso3, year,inc_all=e_inc_100k,  e_inc_100k_lo, e_inc_100k_hi,
+                notif_all=c_newinc_100k,
+                c_cdr, c_cdr_lo, c_cdr_hi)%>%as.data.table()
 
 load(here("data/whokey.Rdata"))
+
 whoz <- c("AFR", "AMR", "EMR", "EUR", "SEA", "WPR")
 whozt <- c(
   "Africa", "The Americas",
@@ -29,6 +33,8 @@ whokeyshort <- rbind(
   whokeyshort,
   data.table(g_whoregion = "Global", region = "Global")
 )
+
+save(whokey,file=here("data/whokey.RData"))
 
 load(here("data/LEu5.Rdata")) # single year under five life expectancy
 ## === utility functions
@@ -128,40 +134,17 @@ load(here("data/LEu5.Rdata")) # single year under five life expectancy
 source(here("R/utilities/modelfunctions.R"))
 source(here("R/utilities/parameters.R"))
 
-
-# ## === parameter values and samplers
-# source(here("R/utilities/parameters.R"))
-
-
 ## === calculations
 ## conventions:
 ## prepend rslt_ for results
 ## postpend _sq for status quo (current BCG coverage)
 ## postpend _cf for counterfactual (no BCG)
-source(here("R/utilities/calculations.R"))
+source(here("R/utilities/calculations.R")) # generates D data.table
 
 # save PSA samples
-save(D, file = here("outputs/PSA.RData"))
-
-
+save(D, file = here("outputs/PSA.RData")) # 
 
 load(here("outputs/PSA.RData"))
-
-# data for percapita deaths
-per_cap_deaths <- D%>%select(iso3, iter,matches("rslt_tb_deaths|rslt_tbn_deaths|rslt_tbm_deaths"))%>%
-  pivot_longer(cols=-c("iter", "iso3"), names_to = "variable")%>%as.data.table()
-per_cap_deaths[, type:= ifelse(str_detect(variable, "cf"), "cf", "sq")]
-per_cap_deaths[, variable:= gsub("rlst_", "", variable)]
-per_cap_deaths[, variable:= gsub("_cf|_sq", "", variable)]
-per_cap_deaths <- dcast(per_cap_deaths,
-                        iter + iso3 + variable ~ type,value.var = "value")%>%
-  mutate(av=cf-sq)%>% select(-c("sq", "cf"))%>%as.data.table()
-
-deaths_per_cap <- merge(per_cap_deaths, whokey, by = "iso3")%>%
-  group_by(g_whoregion, region,iso3,variable)%>%
-  summarise(av= mean(av, na.rm=TRUE))%>%mutate(type="percapita")%>%
-  as.data.table()
-
 
 
 ## === aggregations and outputs
@@ -184,7 +167,7 @@ CEA <- D[, .(
     uc_capital_ave +
     ucost_proc_bcg) # TODO vax prep and inject is 25% of all labour cost?
 ),
-by = c("iso3", "source")
+by = iso3
 ]
 
 
@@ -196,23 +179,10 @@ CEA <- merge(CEA, whokey, by = "iso3")
 CEA <- CEA[!is.na(ENB30)]
 
 ## ICER plot
-CEA$iso3 <- factor(CEA$iso3, levels = unique(CEA[order(ICER)]$iso3), ordered = TRUE)
-
-# save(CEA, file = here("outputs/CEA.RData"))
-# load(here("outputs/CEA.RData"))
-
-
-
 
 thresholds <- c(0.3, 0.5, 1.0)  
-all_labels <- c("ICER < 0.3 GDP","ICER >= 0.3 GDP", 
-                "0.3 GDP", "0.5 GDP","1 GDP")
-all_shapes <- c(19, 1, 3, 3, 3) # shape types
-all_colors <- c("black", "black", 2, 4, 5) # colors
-
 
 CEA <- CEA%>%
-  #distinct(iso3, GDP, region, .keep_all = TRUE) |> 
   crossing(threshold = thresholds)%>%
   mutate(legend_label = paste0(threshold," GDP"))%>% 
   mutate(legend_label= factor(legend_label,
@@ -221,55 +191,26 @@ CEA <- CEA%>%
                 ICER_Label= ifelse(ICER < 0.3 * GDP,"ICER < 0.3 GDP", "ICER >= 0.3 GDP" ))%>%
   left_join(gdp_inc_le_costs%>%
                filter(cov_cat=="WUENIC")%>%
-                            dplyr::select(iso3, incbest,BCG=bcg_coverage,CDR=cdr,
+                            dplyr::select(iso3, inc_u5=incbest,
+                                          notif_u5=notif,
+                                          BCG=bcg_coverage,CDR=cdr,
                                           ucvax=uc_tot_vax_delv_ave,
                                           uctb=ucost_dstb.m, 
                                           uctbm=ucost_tbm.m),
                           by= "iso3")%>%
-  inner_join(tbinc, by="iso3")%>%as.data.table()
+  inner_join(tbinc, by="iso3")%>%
+  
+  mutate(CV1 = 0.05, CV2 = 0.1, CV3 = 0.15,
+    z_score = qnorm(g / (g + u), mean = 0, sd = 1)) %>%
+  mutate(
+    Bf1 = round(100 * z_score * CV1, 1),
+    Bf2 = round(100 * z_score * CV2, 1),
+    Bf3 = round(100 * z_score * CV3, 1))%>%
+as.data.table()
   
 save(CEA, file = here("outputs/CEA.RData"))
-load(here("outputs/CEA.RData"))
 
 
-
-ggplot(CEA[ICER > 0], aes(iso3, ICER)) +
-  geom_point(aes(shape = ICER_Label, col = ICER_Label), 
-             size = 1.5) +
-  geom_point(aes(y = ICER_val,shape = legend_label,
-                 col =legend_label),size = 1.5) +
-  scale_shape_manual(name = "Threshold",
-                     values = setNames(all_shapes, all_labels)) +
-  scale_color_manual(name = "Threshold",
-                     values = setNames(all_colors, all_labels)) +
-  scale_y_log10(labels = scales::comma) +
-  coord_flip() +
-  facet_wrap(~region, scales = "free") +
-  theme_linedraw()+
-  theme(legend.position = "top",
-        legend.box.spacing = unit(0, "pt"),   # no gap between legend and plot
-        legend.margin = margin(0, 0, 0, 0),   # no internal padding in legend
-        plot.margin = margin(0, 5, 5, 5))+
-  xlab("Country ISO3 code") +
-  ylab("Incremental cost-effectiveness ratio (USD/DALY)")
-
-
-ggsave(file = here("plots/cea_ICER_iso3.png"), w = 9, h = 8)
-
-
-## ENB plot
-#CEA$iso3 <- factor(CEA$iso3, levels = CEA[order(ENB30)]$iso3, ordered = TRUE)
-
-ggplot(CEA, aes(reorder(iso3, ENB30), ENB30)) +
-  geom_hline(yintercept = 0, col = 2) +
-  geom_point() +
-  coord_flip() +
-  facet_wrap(~region, scales = "free") +
-  theme_linedraw() +
-  xlab("Country ISO3 code") +
-  ylab("Expected net benefit (USD)")
-
-ggsave(file = here("plots/cea_ENB_iso3.png"), w = 9, h = 8)
 
 ## TODO buffers
 CEA[, summary(ENB30)]
@@ -278,84 +219,72 @@ CEA[, summary(ENB30 / (ENB30 + u))]
 CEA[, summary(qnorm(ENB30 / (ENB30 + u)))]
 
 
-CEA_BUF <- CEA %>%
-  inner_join(
-    gdp_inc_le_costs %>%
-      select(Country = country, iso3) %>%
-      distinct(iso3, .keep_all = TRUE),
-    by = "iso3"
-  ) %>%
-  # filter(ENB30>0)%>%
-  mutate(
-    CV1 = 0.05, CV2 = 0.1, CV3 = 0.15,
-    z_score = qnorm(g / (g + u), mean = 0, sd = 1)
-  ) %>%
-  mutate(
-    Bf1 = round(100 * z_score * CV1, 1),
-    Bf2 = round(100 * z_score * CV2, 1),
-    Bf3 = round(100 * z_score * CV3, 1)
-  )
-
-save(CEA_BUF, file = here("outputs/CEA_BUF.RData"))
-
-load(here("outputs/CEA_BUF.RData")) #datawith buffersz
-
-ft <- CEA_BUF %>%
-  filter(Bf1 > 0, !is.na(Bf1), ENB30 > 0) %>%
-  select(
-    Region = g_whoregion,
-    Country, ENB = ENB30, ICER, `Buffer 1` = Bf1,
-    `Buffer 2` = Bf2, `Buffer 3` = Bf3
-  ) %>%
-  mutate(
-    ENB = round(ENB, 1),
-    ICER = round(ICER, 0)
-  ) %>%
-  as.data.frame() %>%
-  make_flextable()
-
-
-## Save to word document TODO missing package:: 
-doc <- read_docx() |>
-  body_add_flextable(value = ft) |>
-  body_add_par(" ", style = "Normal") # optional spacing
-
-print(doc, target = "outputs/buffer_sz.docx")
-
-## buffer plot
-tmp <- CEA_BUF[ENB30 > 0 & z_score > 0]
-summary(tmp)
-tmpm <- melt(tmp[, .(iso3, region, GDP, Bf1, Bf2, Bf3)],
-  id = c("iso3", "region", "GDP")
-)
-tmpm[, var := fcase(
-  variable == "Bf1", "CV = 5%",
-  variable == "Bf2", "CV = 10%",
-  variable == "Bf3", "CV = 15%"
-  )]
-tmpm$var <- factor(tmpm$var,
-  levels = c("CV = 5%", "CV = 10%", "CV = 15%"),
-  ordered = TRUE
-)
-
-
-ggplot(tmpm, aes(reorder(iso3, value), value,color=var)) + # ordering values
-  geom_point() + ylim(0, NA) +
-  coord_flip() +
-  facet_wrap(~region, scales = "free") +
-  theme_linedraw() +
-  scale_color_brewer(palette = "Dark2") + 
-  theme(legend.position = "top",
-        legend.box.spacing = unit(0, "pt"),   # no gap between legend and plot
-        legend.margin = margin(0, 0, 0, 0),   # no internal padding in legend
-        plot.margin = margin(0, 5, 5, 5))+
-  xlab("Country ISO3 code") +
-  ylab("Optimal buffer as proportion of expected demand (%)")+
-  theme(legend.position = 'top',legend.title = element_blank()) 
-
-ggsave(file = here("plots/f_buffer_sz_cntrs.png"), w = 9, h = 8)
-
 ## TODO global and regional total outputs, e.g.:
+
+
+#====data for percapita deaths======
+per_cap_deaths <- D%>%
+  dplyr::select(iso3, iter,matches("rslt_tb_deaths|rslt_tbn_deaths|rslt_tbm_deaths"))%>%
+  pivot_longer(cols=-c("iter", "iso3"), names_to = "variable")%>%
+  as.data.table()
+
+per_cap_deaths[, type:= ifelse(str_detect(variable, "cf"), "cf", "sq")]
+per_cap_deaths[, variable:= gsub("rlst_", "", variable)]
+per_cap_deaths[, variable:= gsub("_cf|_sq", "", variable)]
+per_cap_deaths <- dcast(per_cap_deaths,
+                        iter + iso3 + variable ~ type,value.var = "value")%>%
+  mutate(av=cf-sq)%>% select(-c("sq", "cf"))%>%as.data.table()
+
+deaths_per_cap <- merge(per_cap_deaths, whokey, by = "iso3") %>%
+  group_by(g_whoregion, region,iso3,variable)%>%
+  summarise(av= mean(av, na.rm=TRUE))%>% filter(!is.na(av))|>
+  mutate(type="percapita_death_averted")%>%
+  as.data.table()
+
+
+##=
+per_cap_ave_inc <- D%>%
+  select(iso3, iter,matches("rslt_inc|rslt_tbminc"))%>%
+  pivot_longer(cols=-c("iter", "iso3"), names_to = "variable")%>%
+  as.data.table()
+
+per_cap_ave_inc[, type:= ifelse(str_detect(variable, "cf"), "cf", "sq")]
+per_cap_ave_inc[, variable:= gsub("rlst_", "", variable)]
+per_cap_ave_inc[, variable:= gsub("_cf|_sq", "", variable)]
+
+per_cap_ave <- dcast(per_cap_ave_inc,
+                        iter + iso3 + variable ~ type,value.var = "value")%>%
+  mutate(av=cf-sq)%>% select(-c("sq", "cf"))%>%
+  group_by(variable, iso3)%>%
+  summarise(av= mean(av, na.rm=TRUE))%>% filter(!is.na(av))|>
+  mutate(type="percapita_inc_averted")%>%
+  bind_rows(per_cap_deaths%>%
+               group_by(variable, iso3)%>%
+               summarise(av= mean(av, na.rm=TRUE),
+                         type="percapita_death_averted"))%>%
+  as.data.table()
+  
+save(per_cap_ave, file=here("outputs/per_cap_ave.RData"))
+
+per_cap_ave %>%
+  group_by(variable) %>%
+  slice_max(av, n = 1, with_ties = FALSE) %>%
+  mutate(Descreption=paste0("Max avertion is in ",iso3),
+         variable= gsub("rslt_", "", variable))%>%
+  mutate(variable= paste0("Percapita avertion in ", variable))%>%
+  select(-iso3, -type, value=av)
+
+
+per_cap_ave %>%
+  group_by(variable) %>%
+  slice_min(av, n = 1, with_ties = FALSE) %>%
+  mutate(Descreption=paste0("Min avertion is in ",iso3),
+         variable= gsub("rslt_", "", variable))%>%
+  mutate(variable= paste0("Percapita avertion in ", variable))%>%
+  select(-iso3, -type,value=av)
+
+
+
 
 ## extract results, compute differences, reformat
 keep <- grep("rslt", names(D), value = TRUE)
@@ -372,15 +301,18 @@ output_table <- dcast(output_table,
   value.var = "value"
 )
 
-
 ## averted
 output_table[, av := cf - sq]
 
 # deaths pop level
-death_absolute <-output_table[is.finite(av), .(av = sum(av, na.rm = TRUE)),by = .(iso3, variable)]%>%
-  select(iso3,variable, av)%>%filter(grepl("deaths", variable))%>%
+death_absolute <- output_table[is.finite(av), .(av = sum(av, na.rm = TRUE)),
+                              by = .(iso3, variable)]%>%
+  select(iso3,variable, av)%>%
+  filter(grepl("deaths", variable))%>%
   mutate(type= "absolute")%>%
-  inner_join(whokey, by = "iso3")%>%as.data.table()
+  inner_join(whokey, by = "iso3")%>%
+  distinct(iso3, variable, .keep_all=TRUE)%>%
+as.data.table()
 
 deaths <- rbind(deaths_per_cap, death_absolute)
 save(deaths, file=here("outputs/deaths.RData"))
@@ -389,20 +321,27 @@ load(here("outputs/deaths.RData"))
 load(here("outputs/CEA.RData"))
 
 ## global TODO NaNs?
+
 output_table <- output_table[is.finite(av), .(
-  cf = sum(cf), sq = sum(sq), av = sum(av)
+  cf = sum(cf), 
+  sq = sum(sq), 
+  av = sum(av)
 ),
 by = .(iter, variable)
 ]
 
+
 ## hi/lo & reshape
+
 eps <- 0.025
 output_table <- melt(output_table,
   id = c("iter", "variable")
 ) # TODO better var names
 
 output_table <- output_table[, .(
-  mid = mean(value), lo = quantile(value, eps), hi = quantile(value, 1 - eps)
+  mid = mean(value), 
+  lo = quantile(value, eps), 
+  hi = quantile(value, 1 - eps)
 ), by = .(variable, variable.1)]
 
 output_table <- dcast(output_table,
@@ -413,8 +352,8 @@ output_table <- dcast(output_table,
 ## change units to millions for cost and health, BCG dosese
 fac <- 1e6
 output_table[
-  variable %in% c("rslt_att_cost", "rslt_cost",
-                  "rslt_bcg_doses", "rslt_health"),
+  variable %in% c("rslt_att_cost","rslt_attm_cost", "rslt_cost",
+                  "rslt_bcg_cost", "rslt_bcg_doses", "rslt_health","rslt_ly_tb"),
   `:=`(
     mid_sq = mid_sq / fac, lo_sq = lo_sq / fac, hi_sq = hi_sq / fac,
     mid_cf = mid_cf / fac, lo_cf = lo_cf / fac, hi_cf = hi_cf / fac,
@@ -437,6 +376,78 @@ output_table
 
 fwrite(output_table, file = here("outputs/output_table.csv"))
 
-# Regional - this should come after the output_table 
+
+#=====Regional - this should come after the output_table=====
+
+keep <- grep("rslt", names(D), value = TRUE)
+keep <- c("who_region","iso3", "iter", "Pop", unique(keep))
+
+out_tab_r <- D[, ..keep]
+out_tab_r <- melt(out_tab_r, id = c("iter", "iso3","who_region", "Pop"))
+out_tab_r[, type := ifelse(grepl("cf", variable), "cf", "sq")]
+out_tab_r[, variable := gsub("rlst_", "", variable)]
+out_tab_r[, variable := gsub("_cf|_sq", "", variable)]
+out_tab_r[, value := value * Pop]
+out_tab_r <- dcast(out_tab_r,
+                   iter + iso3 + variable + who_region~ type,
+                   value.var = "value")
+
+
+## averted
+out_tab_r[, av := cf - sq]
+
+save(out_tab_r, file= here("outputs/out_tab_r.RData"))
+
+
+out_r_aggr <- out_tab_r[is.finite(av), .(
+  cf = sum(cf), sq = sum(sq), av = sum(av)),
+  by = .(iter, variable, who_region)]
+
+## hi/lo & reshape
+eps <- 0.025
+out_r_aggr <- melt(out_r_aggr,
+                   id = c("iter","who_region", "variable")
+) # TODO better var names
+
+out_r_aggr <- out_r_aggr[, .(
+  mid = mean(value), lo = quantile(value, eps), hi = quantile(value, 1 - eps)
+), by = .(who_region,variable, variable.1)]
+
+out_r_aggr <- dcast(out_r_aggr,
+                    who_region+variable ~ variable.1,
+                    value.var = c("mid", "lo", "hi"))
+
+## change units to millions for cost and health, BCG dosese
+fac <- 1e6
+out_r_aggr[
+  variable %in% c("rslt_att_cost","rslt_attm_cost", "rslt_cost",
+                  "rslt_bcg_cost", "rslt_bcg_doses", "rslt_health",
+                  "rslt_ly_tb"),
+  `:=`(
+    mid_sq = mid_sq / fac, lo_sq = lo_sq / fac, hi_sq = hi_sq / fac,
+    mid_cf = mid_cf / fac, lo_cf = lo_cf / fac, hi_cf = hi_cf / fac,
+    mid_av = mid_av / fac, lo_av = lo_av / fac, hi_av = hi_av / fac
+  )
+]
+
+
+
+
+## format numbers, add brackets
+out_r_aggr[
+  ,
+  c("sq_txt", "cf_txt", "av_txt") := .(
+    brkt(mid_sq, lo_sq, hi_sq),
+    brkt(mid_cf, lo_cf, hi_cf),
+    brkt(mid_av, lo_av, hi_av)
+  )
+]
+out_r_aggr
+
+fwrite(out_r_aggr, file = here("outputs/output_table_who.csv"))
+
+
+
+
 source("R/create_outs4who_regi.R") # replicates the global analysis for who_regions
 
