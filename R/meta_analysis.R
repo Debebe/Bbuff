4233a0ffd9e93d925ccf666638cdfe6d63e6c1cc
Debebe <debebesh@gmail.com>
2025-11-14 18:39:19 +0000

    adjusting left margins for stiched forest plots

--- R/meta_analysis.R
+++ R/meta_analysis.R
@@ -1,13 +1,20 @@
 ## meta-analysis for BCG conditional efficacy against TBM
 library(here)
 library(meta)
+library(data.table)
+library(readxl)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(cowplot)  # for draw_label
+
 
 ## copying data from google doc into here manually
 severe_TBM <- data.table(
   study = c(
-    "Sumaya et al (1975)", "Doerr et al (1995)", "Paganini et al (2000)",
-    "Degefie et al (2003)", "Kumar et al (2005)", "VanWell et al (2009)",
-    "Tinsa et al (2010)"
+    "Sumaya, 1975", "Doerr, 1995", "Paganini, 2000",
+    "Degefie, 2003", "Kumar, 2005", "VanWell, 2009",
+    "Tinsa, 2010"
   ),
   n_sever_sequalae = c(7, 4, 9, 9, 60, 77, 1),
   n_sequalae = c(13, 4, 19, 9, 107, 217, 4),
@@ -41,8 +48,11 @@ png(
   bg = "white", res = 250
 )
 forest(m,
+      leftcols = c("studlab","n", "event"),
+      leftlabs = c("Study", "TBM", "Severe sequelae"),
   fontsize = 8,
-  xlab = "Proportion with severe sequelae, 95%CI"
+  fontfamily = "serif", 
+  xlab = "Proportion with TBM sequelae (severe)"
   )
 dev.off()
 
@@ -53,9 +63,9 @@ bcg_TBM<- read_excel("data/bcg_tb_meningitis.xlsx") %>%
   pivot_wider(names_from = vac, 
               values_from = c(TB, MTB), names_prefix = "vac") %>%
   filter(TB_vac1>0)%>%
-  mutate(study= case_when(grepl("Chicago", study) ~"Rosenthal,1945", 
+  mutate(study= case_when(grepl("Chicago", study) ~"Rosenthal, 1945", 
                           grepl("Trub", study) ~"Trub, 1970",
-                          grepl("Chandra",study) ~ "Chandra,1975",
+                          grepl("Chandra",study) ~ "Chandra, 1975",
                           grepl("Tidjani", study) ~ "Tidjani, 1992",
                           grepl("Mahomed", study) ~ "Mahomed, 2006",
                           TRUE ~study))
@@ -97,7 +107,76 @@ forest(
   fontsize = 8,        
   fontfamily = "serif"   # Font family ("Arial", "Times", "Helvetica")
 )
+
+
+
+
+library(cowplot)
+library(here)
+library(magick)
+
+
+
+png(here("plots/forest_a.png"), width = 18, height = 8, units = "cm", res = 250)
+forest(
+  m1,
+  font=8,
+  smlab = "", # to remove title above the plots
+  common = FALSE,
+  comb.random = TRUE,
+  text.random = "Random-effects RR",
+  xlab = "Risk Ratio (RR)",
+  leftcols = c("studlab", "event.e", "n.e", "event.c", "n.c"),
+  leftlabs = c("Study", "TBM", "TB", "TBM", "TB"),
+  label.e= "Vaccinated",
+  label.c= "Unvaccinated",
+  digits = 2,
+  colgap.left = "0.5cm",
+  fontsize = 8,        
+  fontfamily = "serif"   
+)
 dev.off()
+
+# Save each forest plot separately
+png(here("plots/forest_b.png"), width = 18, height = 8, units = "cm", res = 250)
+forest(m,font=8.5,
+       common = FALSE,
+       leftcols = c("studlab","n", "event"),
+       leftlabs = c("Study", "TBM", "Sequelae"),
+       fontsize = 8.5,
+       fontfamily = "serif", 
+       xlab = "Severe sequelae (proportion)")
+dev.off()
+
+
+
+p1 <- ggplot() +
+  annotation_custom(imgA_grob, xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf) +
+  theme_void() +
+  theme(plot.margin = margin(0,0,0,0))
+
+p1 <- ggdraw(p1) +
+  draw_label("a", x = 0.05, y = 0.95, fontface = "bold", fontfamily = "serif", size = 10, hjust = 0, vjust = 1)
+
+p2 <- ggplot() +
+  annotation_custom(imgB_grob, xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf) +
+  theme_void() +
+  theme(plot.margin = margin(0,0,0,0))
+
+p2 <- ggdraw(p2) +
+  draw_label("b", x = 0.05, y = 0.95, fontface = "bold", fontfamily = "serif", size = 10, hjust = 0, vjust = 1)
+
+combined <- plot_grid(p1, p2, ncol=1)
+
+ggsave(
+  filename = here("plots/forest_combined.png"),
+  plot = combined,
+  width = 17.5, height = 16,
+  units = "cm", dpi = 250, bg = "white"
+)
+
+
+
 
 
 metabin(10, 20, 15, 20, sm = "OR") 
