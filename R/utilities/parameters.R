28902e8fa2aaf8833519d6d8206b609bb3f1b65c
Debebe <debebesh@gmail.com>
2025-12-04 08:48:53 +0000

    cleaned parameter values

--- R/utilities/parameters.R
+++ R/utilities/parameters.R
@@ -1,7 +1,5 @@
 ## work to generate the parameters distributions & samplers
-# some parameters
-bcg_eff_tb <- 0.62
-bcg_eff_tbm <- 0.73 # 0.73 (0.67 - 0.79), 0.69(0.60- 0.76 in Asia see Trunz
+
 
 bcg_eff_tb.m  <- 0.63
 bcg_eff_tb.lo <- 0.49
@@ -12,47 +10,75 @@ bcg_eff_tb.hi <- 0.81
 # bcg_eff_tbm.hi <- 0.79
 
 
-bcg_eff_tbm.m  <- 0.58
-bcg_eff_tbm.lo <- 0.20
-bcg_eff_tbm.hi <- 1.74
+bcg_haz_tbm.m  <- 0.58 #is hazard not efficacy
+bcg_haz_tbm.lo <- 0.20
+bcg_haz_tbm.hi <- 1.74
+
+# bcg_haz_tb <- 1 - bcg_eff_tb
+# bcg_haz_tbm <- 1 - bcg_eff_tbm
 
 
-bcg_haz_tb <- 1 - bcg_eff_tb
-bcg_haz_tbm <- 1 - bcg_eff_tbm
 
-cfr_treat <- 0.019 # 1·9 (0·5–7·1) tb-mortality treated
-cfr_utreat <- 0.436 # 43·6 (36·8–50·6) tb-mort untreated
-ucost_proc_bcg <- 0.1205 # for serum institute India- 2023
 disc_rate <- 0.03
 
+cfr_treat_tb.m <- 0.019 # 1·9 (0·5–7·1) tb-mortality treated
+cfr_treat_tb.l <- 0.005 # 1·9 (0·5–7·1) tb-mortality treated
+cfr_treat_tb.h <- 0.071 # 1·9 (0·5–7·1) tb-mortality treated
 
-#post_tb_dis_wt <- 0.04
-post_tb_hrqol <- 0.91 # 0.87- 0.95 based on from https://pmc.ncbi.nlm.nih.gov/articles/PMC8080025/
+cfr_utreat_tb.m <- 0.436 # 43·6 (36·8–50·6) tb-mort untreated
+cfr_utreat_tb.l <- 0.368 # 43·6 (36·8–50·6) tb-mort untreated
+cfr_utreat_tb.h <- 0.506 # 43·6 (36·8–50·6) tb-mort untreated
 
-post_tb_hrqol_dur3 <- 1-0.041 #Tomeny
+
+cfr_treat_tbm.m <- 0.193 # 19·3% (95% CI 14·0–26·1) tbm-mortality treated
+cfr_treat_tbm.l <- 0.140 
+cfr_treat_tbm.h <- 0.261 
+
+cfr_untreat_tbm <- 1 
+
+
+post_tb_hrqol_dur3 <- 1-0.041 #from Tommeny
 post_tb_hrqol_post3 <- 1-0.025
 
-post_tb_mort_hz <- 1.78
 
-tbm_mort_hz <- 1.79 # TODO guess
-# tbm_hrqol     <- 0.60   # TODO  guess
-cfr_treat_tbm <- 0.193 # 19·3% (95% CI 14·0–26·1)
-cfr_untreat_tbm <- 1 # 19·3% (95% CI 14·0–26·1)
+post_tb_mort_hz.m <- 1.78 # posttb morthaz 1.78, 1.61, 1.98
+post_tb_mort_hz.l <- 1.61 # posttb morthaz 1.78, 1.61, 1.98
+post_tb_mort_hz.h <- 1.98 # posttb morthaz 1.78, 1.61, 1.98
 
-# additional inputs
-#prop_tbm <- 0.034
+post_tbm_mort_hz.m <- 1.79 # posttbm morthaz 1.79, 1.09, 2.95
+post_tbm_mort_hz.l <- 1.09 # posttbm morthaz 1.79, 1.09, 2.95
+post_tbm_mort_hz.h <- 2.95 # posttbm morthaz 1.79, 1.09, 2.95
+
+
 prop_tbm.ave <- 0.034
-prop_tbm.lo <-0.0187
+prop_tbm.lo <- 0.0187
 prop_tbm.hi <- 0.06
 
-prop_sev_seq <- 0.56 # 0.56[0.47-0. 63] meta-analysis
-prop_nsev_seq <- 0.44 # 0.44 [0.37, 0.53]
-prop_mild_seq <- 0.70 * prop_nsev_seq
-prop_mod_seq <- 0.30 * prop_nsev_seq
+prop_sev_seq.m <- 0.56 # 0.56[0.47-0. 63] meta-analysis
+prop_sev_seq.l <- 0.47 # 0.56[0.47-0. 63] meta-analysis
+prop_sev_seq.h <- 0.63 # 0.56[0.47-0. 63] meta-analysis
 
-tbm_hrqol_mil_seq <- 1 - 0.031 # 0·031 (0·018–0·050) #Global burden of disease, 2013
-tbm_hrqol_mod_seq <- 1 - 0.203 # 0·203 (0·134–0·290)
-tbm_hrqol_sev_seq <- 1 - 0.542 # 0·542 (0·374–0·702)
+
+prop_mild_disab <- 0.70 #proportion of nonsever tbm that is mild
+
+# prop_mild_seq <- prop_mild_disab * (1-prop_sev_seq)
+# prop_mod_seq   <-(1-prop_mild_disab) * (1-prop_sev_seq)
+
+tbm_dis_wt_mild.m <- 0.031
+tbm_dis_wt_mild.l <- 0.018
+tbm_dis_wt_mild.h <- 0.050
+
+tbm_dis_wt_mod.m <- 0.203
+tbm_dis_wt_mod.l <- 0.134
+tbm_dis_wt_mod.h <- 0.290
+
+tbm_dis_wt_sev.m <- 0.542
+tbm_dis_wt_sev.l <- 0.374
+tbm_dis_wt_sev.h <- 0.702
+
+# tbm_hrqol_mil_seq <- 1 - 0.031 # 0·031 (0·018–0·050) #Global burden of disease, 2013
+# tbm_hrqol_mod_seq <- 1 - 0.203 # 0·203 (0·134–0·290)
+# tbm_hrqol_sev_seq <- 1 - 0.542 # 0·542 (0·374–0·702)
 
 LEref <- 88.871895
 
